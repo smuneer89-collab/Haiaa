cd377997e1da6ece0e84807c46861240ec0d25fa
smuneer89-collab <smuneer89@gmail.com>
2026-09-03 12:16:53 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
    • طلبات Firebase والخطوط: تمر مباشرة بلا تخزين ← لا تتعطّل المزامنة
    • بلا إنترنت: يرجع لآخر نسخة مخزّنة تلقائياً
 */
-const CACHE = 'husain-v39-stable-cultural-survey-link';
+const CACHE = 'husain-v40-cultural-survey-meeting-reliable';
 
 const APP_SHELL = [
   './',
